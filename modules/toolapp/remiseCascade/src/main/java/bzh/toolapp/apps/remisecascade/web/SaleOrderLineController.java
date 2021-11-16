25e70cfcfcd41e495992ca31b5a10116430fd278
Gabriel Barbier <amelioretonparent@gmail.com>
2021-11-16 16:40:32 +0100

    Essai de prise en compte de la double remise au niveau des listes de prix pour un document de vente (à la ligne)

--- modules/toolapp/remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/web/SaleOrderLineController.java
+++ modules/toolapp/remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/web/SaleOrderLineController.java
@@ -1,5 +1,12 @@
 package bzh.toolapp.apps.remisecascade.web;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.axelor.apps.base.db.repo.PriceListLineRepository;
 import com.axelor.apps.sale.db.SaleOrder;
 import com.axelor.apps.sale.db.SaleOrderLine;
 import com.axelor.apps.sale.service.saleorder.SaleOrderLineService;
@@ -10,49 +17,117 @@ import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Context;
 import com.google.inject.Singleton;
-import java.math.BigDecimal;
-import java.util.Map;
 
 @Singleton
 public class SaleOrderLineController {
 
-  /**
-   * Overrides method "compute" from SaleOrderLineController in project axelor-sale.
-   *
-   * @param request
-   * @param response
-   */
-  public void compute(ActionRequest request, ActionResponse response) {
-    Context context = request.getContext();
+	private final Logger logger = LoggerFactory.getLogger(SaleOrderLineService.class);
 
-    SaleOrderLine saleOrderLine = context.asType(SaleOrderLine.class);
+	/**
+	 * Overrides method "compute" from SaleOrderLineController in project axelor-sale.
+	 *
+	 * @param request
+	 * @param response
+	 */
+	public void compute(final ActionRequest request, final ActionResponse response) {
+		final Context context = request.getContext();
 
-    SaleOrder saleOrder = Beans.get(SaleOrderLineService.class).getSaleOrder(context);
+		final SaleOrderLine saleOrderLine = context.asType(SaleOrderLine.class);
 
-    try {
-      compute(response, saleOrder, saleOrderLine);
-    } catch (Exception e) {
-      TraceBackService.trace(response, e);
-    }
-  }
+		final SaleOrder saleOrder = Beans.get(SaleOrderLineService.class).getSaleOrder(context);
 
-  private void compute(ActionResponse response, SaleOrder saleOrder, SaleOrderLine orderLine)
-      throws AxelorException {
+		try {
+			this.compute(response, saleOrder, saleOrderLine);
+		} catch (final Exception e) {
+			TraceBackService.trace(response, e);
+		}
+	}
 
-    Map<String, BigDecimal> map =
-        Beans.get(SaleOrderLineService.class).computeValues(saleOrder, orderLine);
+	private void compute(final ActionResponse response, final SaleOrder saleOrder, final SaleOrderLine orderLine)
+			throws AxelorException {
 
-    map.put("price", orderLine.getPrice());
-    map.put("inTaxPrice", orderLine.getInTaxPrice());
-    map.put("companyCostPrice", orderLine.getCompanyCostPrice());
-    map.put("discountAmount", orderLine.getDiscountAmount());
+		final Map<String, BigDecimal> map =
+				Beans.get(SaleOrderLineService.class).computeValues(saleOrder, orderLine);
 
-    response.setValues(map);
-    response.setAttr(
-        "priceDiscounted",
-        "hidden",
-        map.getOrDefault("priceDiscounted", BigDecimal.ZERO)
-                .compareTo(saleOrder.getInAti() ? orderLine.getInTaxPrice() : orderLine.getPrice())
-            == 0);
-  }
+		map.put("price", orderLine.getPrice());
+		map.put("inTaxPrice", orderLine.getInTaxPrice());
+		map.put("companyCostPrice", orderLine.getCompanyCostPrice());
+		map.put("discountAmount", orderLine.getDiscountAmount());
+
+		response.setValues(map);
+		response.setAttr(
+				"priceDiscounted",
+				"hidden",
+				map.getOrDefault("priceDiscounted", BigDecimal.ZERO)
+				.compareTo(saleOrder.getInAti() ? orderLine.getInTaxPrice() : orderLine.getPrice())
+				== 0);
+	}
+
+	public void getDiscount(final ActionRequest request, final ActionResponse response) {
+
+		final Context context = request.getContext();
+		final SaleOrderLine saleOrderLine = context.asType(SaleOrderLine.class);
+		final SaleOrderLineService saleOrderLineService = Beans.get(SaleOrderLineService.class);
+
+		final SaleOrder saleOrder = saleOrderLineService.getSaleOrder(context);
+
+		if ((saleOrder == null) || (saleOrderLine.getProduct() == null)) {
+			return;
+		}
+
+		try {
+
+			Map<String, Object> discounts;
+			if (saleOrderLine.getProduct().getInAti()) {
+				discounts = saleOrderLineService.getDiscountsFromPriceLists(saleOrder, saleOrderLine,
+						saleOrderLineService.getInTaxUnitPrice(saleOrder, saleOrderLine, saleOrderLine.getTaxLine()));
+			} else {
+				discounts = saleOrderLineService.getDiscountsFromPriceLists(saleOrder, saleOrderLine,
+						saleOrderLineService.getExTaxUnitPrice(saleOrder, saleOrderLine, saleOrderLine.getTaxLine()));
+			}
+
+			if (discounts != null) {
+				final BigDecimal price = (BigDecimal) discounts.get("price");
+				if ((price != null)
+						&& (price.compareTo(saleOrderLine.getProduct().getInAti() ? saleOrderLine.getInTaxPrice()
+								: saleOrderLine.getPrice()) != 0)) {
+					if (saleOrderLine.getProduct().getInAti()) {
+						response.setValue("inTaxPrice", price);
+						response.setValue("price",
+								saleOrderLineService.convertUnitPrice(true, saleOrderLine.getTaxLine(), price));
+					} else {
+						response.setValue("price", price);
+						response.setValue("inTaxPrice",
+								saleOrderLineService.convertUnitPrice(false, saleOrderLine.getTaxLine(), price));
+					}
+				}
+
+				if ((saleOrderLine.getProduct().getInAti() != saleOrder.getInAti()) && ((Integer) discounts
+						.get("discountTypeSelect") != PriceListLineRepository.AMOUNT_TYPE_PERCENT)) {
+					response.setValue("discountAmount",
+							saleOrderLineService.convertUnitPrice(saleOrderLine.getProduct().getInAti(),
+									saleOrderLine.getTaxLine(), (BigDecimal) discounts.get("discountAmount")));
+				} else {
+					response.setValue("discountAmount", discounts.get("discountAmount"));
+				}
+				response.setValue("discountTypeSelect", discounts.get("discountTypeSelect"));
+				// manage second discount
+				this.logger.debug("seconde remise, type de sélection = {}, montant = {}",
+						discounts.get("lineSecondDiscountTypeSelect"), discounts.get("lineSecondDiscountAmount"));
+				if ((saleOrderLine.getProduct().getInAti() != saleOrder.getInAti()) && ((Integer) discounts
+						.get("lineSecondDiscountTypeSelect") != PriceListLineRepository.AMOUNT_TYPE_PERCENT)) {
+					response.setValue("secDiscountAmount",
+							saleOrderLineService.convertUnitPrice(saleOrderLine.getProduct().getInAti(),
+									saleOrderLine.getTaxLine(),
+									(BigDecimal) discounts.get("lineSecondDiscountAmount")));
+				} else {
+					response.setValue("secDiscountAmount", discounts.get("lineSecondDiscountAmount"));
+				}
+				response.setValue("secDiscountTypeSelect", discounts.get("lineSecondDiscountTypeSelect"));
+			}
+
+		} catch (final Exception e) {
+			response.setFlash(e.getMessage());
+		}
+	}
 }
