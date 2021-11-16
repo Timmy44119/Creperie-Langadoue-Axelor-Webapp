9c6279bcc0770d04ce362f5f29e7c398fa339cf4
Gabriel Barbier <amelioretonparent@gmail.com>
2021-11-16 23:10:56 +0100

    Mise en place de la double remise globale pour les factures.

--- modules/toolapp/remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/module/RemiseCascadeModule.java
+++ modules/toolapp/remiseCascade/src/main/java/bzh/toolapp/apps/remisecascade/module/RemiseCascadeModule.java
@@ -21,9 +21,11 @@ import com.axelor.app.AxelorModule;
 import com.axelor.apps.base.service.PriceListService;
 import com.axelor.apps.businessproduction.service.SaleOrderLineBusinessProductionServiceImpl;
 import com.axelor.apps.businessproject.service.InvoiceLineProjectServiceImpl;
+import com.axelor.apps.cash.management.service.InvoiceServiceManagementImpl;
 import com.axelor.apps.supplychain.service.SaleOrderComputeServiceSupplychainImpl;
 
 import bzh.toolapp.apps.remisecascade.service.ExtendedInvoiceLineServiceImpl;
+import bzh.toolapp.apps.remisecascade.service.ExtendedInvoiceServiceImpl;
 import bzh.toolapp.apps.remisecascade.service.ExtendedPriceListServiceImpl;
 import bzh.toolapp.apps.remisecascade.service.ExtendedSaleOrderComputeServiceImpl;
 import bzh.toolapp.apps.remisecascade.service.ExtendedSaleOrderLineServiceImpl;
@@ -37,5 +39,6 @@ public class RemiseCascadeModule extends AxelorModule {
 		.to(ExtendedSaleOrderLineServiceImpl.class);
 		this.bind(PriceListService.class).to(ExtendedPriceListServiceImpl.class);
 		this.bind(InvoiceLineProjectServiceImpl.class).to(ExtendedInvoiceLineServiceImpl.class);
+		this.bind(InvoiceServiceManagementImpl.class).to(ExtendedInvoiceServiceImpl.class);
 	}
 }
